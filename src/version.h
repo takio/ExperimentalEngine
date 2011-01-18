73f21e4c79436f2638359c0c4caae02fcef090db
ttakio <ttakio@ymail.com>
2011-01-18 23:29:01 +0100

    modify files

--- src/version.h
+++ src/version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 0;
-	static const long BUILD = 0;
-	static const long REVISION = 0;
+	static const long BUILD = 1;
+	static const long REVISION = 7;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 2;
-	#define RC_FILEVERSION 0,0,0,0
-	#define RC_FILEVERSION_STRING "0, 0, 0, 0\0"
-	static const char FULLVERSION_STRING[] = "0.0.0.0";
+	static const long BUILDS_COUNT = 9;
+	#define RC_FILEVERSION 0,0,1,7
+	#define RC_FILEVERSION_STRING "0, 0, 1, 7\0"
+	static const char FULLVERSION_STRING[] = "0.0.1.7";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 0;
+	static const long BUILD_HISTORY = 1;
 	
 
 }
